93ff475a255956d717ec0d11d852742b915035cb
Rescue <36008727+kabirSohail7@users.noreply.github.com>
2020-01-23 17:56:56 +0530

    Basic without database

---
+++ MySpringMVC/src/com/atos/controller/MyController.java
@@ -0,0 +1,12 @@
+package com.atos.controller;
+
+import org.springframework.stereotype.Controller;  
+import org.springframework.web.bind.annotation.RequestMapping;  
+@Controller
+public class MyController {
+	@RequestMapping("/")  
+    public String display()  
+    {  
+        return "index";  
+    }     
+}
